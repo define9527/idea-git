dd03f8b9315a4d07b61892329f70b3095965f150
define <1340180401@qq.com>
2020-05-17 17:08:25 +0800

    我修改好了

--- src/com/define/git/First.java
+++ src/com/define/git/First.java
@@ -4,5 +4,6 @@ public class First {
     public static void main(String[] args) {
         System.out.println("git演示");
         System.out.println("演示修改111");
+        System.out.println("请在另一个工程内更新");
     }
 }
