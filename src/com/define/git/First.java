c917e077b84a686ee7c87ee9206a3e99227c71f2
define <1340180401@qq.com>
2020-05-17 17:12:50 +0800

    添加还是覆盖

--- src/com/define/git/First.java
+++ src/com/define/git/First.java
@@ -2,8 +2,6 @@ package com.define.git;
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("git演示");
-        System.out.println("演示修改111");
-        System.out.println("请在另一个工程内更新");
+        System.out.println("到底是添加还是覆盖");
     }
 }
